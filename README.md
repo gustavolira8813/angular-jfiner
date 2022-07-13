66042fb061e2ceefc469144d79755f25e1f7279a
gustavolira8813 <gustavolira8813@gmail.com>
2022-07-13 18:57:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jfiner
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jfiner)
---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css'],
+})
+export class ProductAlertComponent implements OnInit {
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+  @Output() rollBack = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {}
+}
